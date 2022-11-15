f8dbc8856729f499ec1f2bb60d2cb2ccf9d6d168
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2022-11-15 22:37:18 +0400

    Add Top Section
    Basic top section. Needs alignment work!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import * as React from 'react';
+import "./App.css";
+import TopSection from "./TopSection"
+import DnDLogo from "./images/dnd-logo.png"
 
+function App() {
+  return (
+    <div className="App">
+      {/* TOP SECTION */}
+      <div className="container">
+        <div className="row mb-1">
+          <div className="col border border-primary rounded mr-3 align-self-center">
+            <img src={DnDLogo} alt="dnd-logo" className="dndlogo"/>
+            <TopSection label="CHARACTER NAME"/>
+          </div>
 
-export default function App(){
-  return(
-    <div></div>
-  )
+          <div className="col-9 border border-primary rounded">
+            <div className="row">
+              <TopSection label="CLASS"/>
+              <TopSection label="LEVEL"/>
+              <TopSection label="BACKGROUND"/>
+            </div>
+
+            <div className="row">
+              <TopSection label="RACE"/>
+              <TopSection label="ALIGNMENT"/>
+              <TopSection label="PLAYER NAME"/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer class="my-4 border-top">
+    <span class="mb-3 mb-md-0 text-muted footertext">Walid Dib</span>
+  </footer>
+    </div>
+  );
 }
 
+export default App;
---
+++ src/TopSection.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function TopSection(props) {
+    return (
+          <div className="col text-center">
+            <input className="form-control"/>
+            <label>{props.label}</label>
+          </div>
+    )
+}
