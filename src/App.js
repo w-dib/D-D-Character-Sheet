21a74e3938dec475c78ed023e7e9c68f2157398d
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2022-11-16 21:21:28 +0400

    Revert "Add Browser Router"
    This reverts commit 3f5297291a48b6f377c9d71ee664ce91650aa4b3.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import "./App.css";
 import TopSection from "./TopSection"
 import DnDLogo from "./images/dnd-logo.png"
 
-<BrowserRouter basename={window.location.pathname || ''}>
-  <Route exact path="/" component={Index} />
-</BrowserRouter>
-
 function App() {
   return (
     <div className="App">
