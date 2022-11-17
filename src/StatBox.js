42f492aea837e1f388cba90d9d056a7bf7a954f6
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2022-11-17 18:26:24 +0400

    Add counter

--- src/StatBox.js
+++ src/StatBox.js
@@ -4,17 +4,52 @@ import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import Button from '@mui/material/Button';
 
 export default function StatBox(props) {
-  
+  const[count, setCount] = React.useState(8)
+
+  const incrementCount = () => {
+    setCount(count+1);
+
+}
+
+const decrementCount = () => {
+  setCount(count-1);  
+}
+
+// const [modifier, setModifier] = React.useState(1)
+
+// const updateModifier = () => {
+//   if (count===1) {
+//     modifier = -1;
+//   }
+//   else if () {
+//     modifier = ??;
+//   }
+//   else if () {
+//     modifier = ??;
+//   }
+//   else if () {
+//     modifier = ??;
+//   }
+//   else if () {
+//     modifier = ??;
+//   }
+// }
     return (
       <div className="col text-center border border-danger rounded mt-2 mb-2">
         <label>{props.stats}</label>
         <div className="d-and-d-statbox-modifier">0</div>
         <div className="row justify-content-center mb-2">
-          <Button size="sm" variant="contained" color="error">
+          <Button 
+          onClick={decrementCount} 
+          size="sm" 
+          variant="contained" 
+          color="error">
             <ArrowBackIosIcon fontSize="small"/>
           </Button>
-          <input className="form-control w-25 my-0 mx-1 h-50" />
-          <Button size="sm" variant="contained" color="error">
+          <input className="form-control text-center w-25 my-0 mx-1 h-50 text" 
+          value={count}
+          />
+          <Button onClick={incrementCount} size="sm" variant="contained" color="error">
             <ArrowForwardIosIcon fontSize="small"/>
           </Button>
         </div>
